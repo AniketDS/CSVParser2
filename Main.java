186e953b243ca9e50cd6a8a63248798644193380
AniketDS <72966122+AniketDS@users.noreply.github.com>
2020-10-18 20:44:50 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		String path = "Csv Parser\\src\\com\\parser\\Data.csv";
+		String path = "src\\com\\parser\\Data.csv";
 		ParseCSV p = new ParseCSV();
 		p.read(path);
 	}
